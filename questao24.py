80d59d6beaabe31e9a0d8b5ad2a8e49d12e8cf39
LuanCantalice <luaancantalice@gmail.com>
2016-09-02 15:39:50 -0300

    Exercicio Prova 2 Bim
    Exercicio Prova 2 Bim

---
+++ questao24.py
@@ -0,0 +1,9 @@
+notaQuant = eval(input(" Qual a quantidade de notas? "))
+nota = eval(input(" Qual as notas? "))
+
+for media in range(1, notaQuant+1):
+    media = notaQuant/nota
+print(media)
+    
+
+    
