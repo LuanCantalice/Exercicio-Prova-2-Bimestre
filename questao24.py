ce836c40012ad8311ebdcc49ac6d2a46a2fd576c
LuanCantalice <luaancantalice@gmail.com>
2016-09-02 17:03:06 -0300

    Delete questao24.py

--- questao24.py
+++
@@ -1,9 +0,0 @@
-notaQuant = eval(input(" Qual a quantidade de notas? "))
-nota = eval(input(" Qual as notas? "))
-
-for media in range(1, notaQuant+1):
-    media = notaQuant/nota
-print(media)
-    
-
-    
