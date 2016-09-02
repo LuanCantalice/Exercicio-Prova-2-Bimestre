4231f085bfb97ac0a1620dc94cf2944726629f67
LuanCantalice <luaancantalice@gmail.com>
2016-09-02 15:15:09 -0300

    Exercicio Prova 2 Bim
    Exercicio Prova 2 Bimestre

---
+++ questao25.py
@@ -0,0 +1,18 @@
+numPessoas = eval(input(" Qual o numero de pessoas na turma? "))
+aux = 0
+
+for media in range(1, numPessoas+1):
+    idade = eval(input(" Digite a sua idade: "))
+    soma = aux + idade
+    aux = soma
+    
+media = soma / numPessoas
+
+if (media >= 0 and media <= 25):
+    print(" Essa turma é jovem ")
+
+elif (media <= 60):
+    print(" Essa turma é adulta ")
+
+elif (media > 60):
+    print(" Essa turma é idosa ")
